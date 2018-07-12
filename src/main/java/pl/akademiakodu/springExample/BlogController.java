38e213f3c2ff25fbd5006a16a65f26b7867274bd
Paulina Wójcik <paulina.wojcikk@o2.pl>
2018-07-12 14:44:22 +0200

    second commit

--- src/main/java/pl/akademiakodu/springExample/BlogController.java
+++ src/main/java/pl/akademiakodu/springExample/BlogController.java
@@ -2,7 +2,9 @@ package pl.akademiakodu.springExample;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
@@ -12,4 +14,17 @@ public class BlogController {
     public String add() {
         return "add";
     }
-}
+
+    @GetMapping("/show")
+    public  String show (@RequestParam String title,
+                         @RequestParam String description,
+                         @RequestParam String author,
+                         ModelMap modelMap ) {
+        Post post = new Post(title,description,author);
+        modelMap.put("post",post);
+        return "show";
+
+
+    }
+    }
+
---
+++ src/main/java/pl/akademiakodu/springExample/Post.java
@@ -0,0 +1,44 @@
+package pl.akademiakodu.springExample;
+
+public class Post {
+
+    private String title;
+    private String description;
+    private String author;
+
+    public Post() {}
+
+    public Post(String title, String description, String author) {
+        this.title = title;
+        this.description = description;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String toString() {
+        return getAuthor()+" "+getTitle()+" "+getDescription();
+    }
+}
